0351e1fe8dd5202fd313eec9b61065cd8f91878a
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-03-29 15:50:27 -0500

    Sprint 4 final stage attempted. First Submisison

--- scripts/index.js
+++ scripts/index.js
@@ -25,15 +25,66 @@ const initialCards = [
   },
 ];
 
+// Dynamic card-addition
+const sectionElements = document.querySelector(".elements");
+const elementsList = document.querySelector(".elements__list");
+const elementsTemplate = document.querySelector("#add-elements").content;
+function getCardElement(data) {
+  let cardElement = elementsTemplate.querySelector(".element").cloneNode(true);
+  // Store card title and image in variables
+  let cardName = cardElement.querySelector(".element__name");
+  let cardImage = cardElement.querySelector(".element__image");
+
+  // Set title, image, and alt to object attributes
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardName.textContent = data.name;
+  return cardElement;
+}
+
+for (let i in initialCards) {
+  elementsList.append(getCardElement(initialCards[i]));
+}
+
+// Nodes
 const editButton = document.querySelector(".profile__button-edit");
 const pageBody = document.querySelector(".page");
-const modalForm = pageBody.querySelector(".modal");
+const modal = pageBody.querySelector(".modal"); // form HTML
+const modalForm = modal.querySelector(".modal__form");
+const modalTitle = modalForm.querySelector(".modal__title");
+const modalDescription = modalForm.querySelector(".modal__description");
 const closeButton = pageBody.querySelector(".modal__close");
+const saveButton = pageBody.querySelector(".modal__save");
 
+const sectionProfile = pageBody.querySelector(".profile");
+const sectionProfileInfo = pageBody.querySelector(".profile__profile-info");
+const sectionProfileInfoHeading = sectionProfileInfo.querySelector(
+  ".profile__profile-heading"
+);
+const sectionProfileInfoSubtitle =
+  sectionProfileInfo.querySelector(".profile__subtitle");
+
+// Click 'edit' button
 editButton.addEventListener("click", function () {
-  modalForm.classList.add("modal_opened");
+  modal.classList.add("modal_opened"); // opens modal
+
+  modalTitle.value = sectionProfileInfoHeading.textContent;
+  modalDescription.value = sectionProfileInfoSubtitle.textContent;
 });
 
+// Click 'close' button in modal
 closeButton.addEventListener("click", function () {
-  modalForm.classList.remove("modal_opened");
+  modal.classList.remove("modal_opened");
+});
+
+// Edit fields in modal form and save to HTML without page-refresh
+modalForm.addEventListener("submit", function handleModalFormSubmit(evt) {
+  evt.preventDefault();
+  // console.log(evt);
+
+  sectionProfileInfoHeading.textContent = modalTitle.value;
+  sectionProfileInfoSubtitle.textContent = modalDescription.value;
+
+  // Close modal
+  modal.classList.remove("modal_opened");
 });
